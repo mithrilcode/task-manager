662fe294c2208a27c82ce07f6a0349c6321c85df
Connor <connor.f.brinkley@gmail.com>
2026-01-30 13:35:43 -0600

    Add layout wrapper to restore visible page content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Tasks from "./pages/Tasks";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind is working 🚀
-      </h1>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-900 text-white">
+        <Routes>
+          {/* Public route */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/tasks" element={<Tasks />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+
+export default App;
 
